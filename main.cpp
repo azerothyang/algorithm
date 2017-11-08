9f5039d3da5c0d3b87ed646d6034c9007d194d52
azerothYang <azerothyangg@gmail.com>
2017-11-08 16:43:58 +0800

    selectionSort

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+using namespace std;
+/**
+ * 选择排序
+ * @param arr 数组
+ * @param n   数组长度
+ */
+void selectionSort(int arr[], int n){
+    for (int i = 0; i < n; ++i) {
+       //寻找i到n最小的数组值的索引，并且
+        int minIndex = i;
+        int tmp = 0;
+        for (int j = i+1; j < n; ++j) {
+            if(arr[i] > arr[j]){
+                minIndex = j;
+                //将当前小值和大值交换位置
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
 
 int main() {
-    long sum = 0;
-    for (long i = 0; i < 127000000; ++i) {
-        sum += i;
-    }
-    std::cout << sum << std::endl;
+    int arr[5] = {3,1,7,2,5};
+    selectionSort(arr, 5);
+
     return 0;
 }
